df582ca3091c1dc73e98ceab4c1960370f463a6a
Victor J. Reventos <victor.reventos1@gmail.com>
2016-03-06 20:43:21 -0600

    Basic Rational number class.

---
+++ src/main/scala/Rational.scala
@@ -0,0 +1,46 @@
+/**
+  * Created by vjames19 on 3/6/16.
+  */
+class Rational(numerator: Int, denominator: Int) {
+
+
+  def this(n: Int) = this(n, 1) // aux constructor
+
+  require(denominator != 0, "Denominator shouldn't be zero")
+
+  private val g = gcd(numerator.abs, denominator.abs)
+  val num = numerator / g
+  val denom = denominator / g
+
+  def +(that: Rational): Rational =
+    new Rational(
+      num * that.denom + that.num * denom,
+      denom * that.denom)
+
+  def +(i: Int): Rational = this.+(new Rational(i))
+
+  def -(that: Rational): Rational = this.+(new Rational(-that.num, that.denom))
+
+  def -(i: Int): Rational = this.-(new Rational(i))
+
+  def *(that: Rational): Rational = new Rational(num * that.num, denom * that.denom)
+
+  def *(i: Int): Rational = this.*(new Rational(i))
+
+
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
+  override def toString() = num + "/" + denom
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Rational =>
+        num == that.num &&
+        denom == that.denom
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(num, denom)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+}
---
+++ src/test/scala/RationalSpec.scala
@@ -0,0 +1,61 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by vjames19 on 3/6/16.
+  */
+class RationalSpec extends FlatSpec with Matchers {
+
+  "A Rational" must "not accept 0 as denominator" in {
+    intercept[IllegalArgumentException] {
+      new Rational(1, 0)
+    }
+  }
+
+  it should "allow using 0 as numerator" in {
+    new Rational(0, 1).num shouldEqual 0
+  }
+
+  it should "allow to instantiate with just the numerator" in {
+    new Rational(2) shouldEqual new Rational(2, 1)
+  }
+
+  it should "simplify the expression" in {
+    new Rational(66, 42) shouldEqual new Rational(11, 7)
+
+    new Rational(10, 2) shouldEqual new Rational(5)
+  }
+
+  it should "add rationals with same denominator" in {
+    new Rational(10, 2) + new Rational(20, 2) shouldEqual new Rational(30, 2)
+    new Rational(10, 2) + new Rational(20, 2) shouldEqual new Rational(15)
+  }
+
+  it should "add with different denominators" in {
+    new Rational(10, 2) + new Rational(20, 3) shouldEqual new Rational(70, 6)
+  }
+
+  it should "add another integer" in {
+    new Rational(1, 2) + 1 shouldEqual new Rational(3, 2)
+  }
+
+  it should "substract" in {
+    new Rational(1, 2) - new Rational(1) shouldEqual new Rational(-1, 2)
+  }
+
+  it should "multiply" in {
+    new Rational(10, 2) * new Rational(20, 3) shouldEqual new Rational(200, 6)
+  }
+
+  it should "multiply another integer" in {
+    new Rational(10, 2) * 2 shouldEqual new Rational(10)
+  }
+
+  it should "follow operator precedence" in {
+    val x = new Rational(10, 2)
+    val y = new Rational(5)
+    val z = new Rational(2, 5)
+
+    x + y * z shouldEqual new Rational(7)
+    (x + y) * z shouldEqual new Rational(4)
+  }
+}
